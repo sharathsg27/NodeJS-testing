ce49eee0d2079e326a312de3be3e37bce3e87189
Sharath Subramanya <blaze@Sharaths-MacBook-Air-2.local>
2019-01-07 17:16:02 +0530

    Cheerio module added 'text -> html' conversion to check http respose data

--- test/app-spec.js
+++ test/app-spec.js
@@ -1,11 +1,21 @@
-var expect = require('chai').expect;
-var rewire = require('rewire');
-var app = rewire('../app');
+const expect = require('chai').expect;
+const rewire = require('rewire');
+const app = rewire('../app');
 const request = require('supertest');
+
+// Cheerio module is used to convert responses in text format to HTML similar to Jquery to manipulate DOM
+const cheerio = require('cheerio');
 
 describe("Dictionary App", function () {
     it("Loads the home page", (done) => {
-        request(app).get('/').expect(200).end(done);
+        request(app).get('/').expect(200).end((err, res) => {
+
+            // DOM manipulation using response text
+            let $ = cheerio.load(res.text);
+            let pageHeading = $("body>h1:first-child").text();
+            expect(pageHeading).to.equal('Skier Dictionary');
+            done();
+        });
     });
 
     describe("Dictionary API", function () {
